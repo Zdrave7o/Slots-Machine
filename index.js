f0a1385e7468fa87e1478142423261a1e02de915
Zdravko Georgiev <zzgzdravko@gmail.com>
2025-04-17 12:45:05 +0300

    Working simple credit system, some styling needed.

--- index.js
+++ index.js
@@ -3,12 +3,38 @@ const reels = document.querySelectorAll(".reel");
 const message = document.getElementById("message");
 const slotMachine = document.querySelector(".slot-machine");
 
+const slotMachineDisplay = document.querySelector("#slot-machine-display");
+const welcomePage = document.querySelector("#mainPageSection");
+const submitBtn = document.querySelector('#submitCredits');
+const availableCreditsDisplay = document.querySelector("#availableCredits");
+let credits = 0;
+
+slotMachineDisplay.style.display = "none";
 
 //array of image source in the reels
 const images = ["Images/seven.png", 
     "Images/lemon.png", 
     "Images/cherries.png"];
 let confettiInterval;
+
+//function to enter the credits and show the slots machine
+function enterCredits(){
+    credits = Number(document.getElementById("creditInput").value);
+    console.log(credits);
+    
+    if(credits < 100){
+        window.prompt("You cannot enter less than 100 credits!");
+    } else{
+        // console.log(credits);//checkout if its working
+        welcomePage.style.display = "none";
+        slotMachineDisplay.style.display = "flex";
+    }
+
+    initializeReels();
+}
+
+submitBtn.addEventListener("click", enterCredits);
+
 
 //initialize reels to show only one image at the start
 function initializeReels(){
@@ -17,7 +43,7 @@ function initializeReels(){
     reels.forEach(reel => {
         reel.innerHTML = `<img src='${images[randomNum]}' alt="default">`; 
     });
-
+    availableCreditsDisplay.textContent = `Available credits: ${credits}`;
 }
 //function to spin each reel
 function spinReel(reel, duration){
@@ -63,35 +89,53 @@ function stopConfetti(){
 
 //main function to start the game
 async function startGame(){
-    message.textContent = "";
+    let bet = Number(document.getElementById("bet").value);
 
-    //reset any previous effects
-    slotMachine.classList.remove("loss", "win");
-    stopConfetti();
+    if (bet > credits) {
+        window.prompt("insufficent funds!");
+    } else if(bet <= 0){
+        window.prompt("You cannot bet less than 1 credit!");
+    } 
+    else {
+        message.textContent = "";
 
-    initializeReels();
+        //reset any previous effects
+        slotMachine.classList.remove("loss", "win");
+        stopConfetti();
 
-    const results = await Promise.all([
-        spinReel(reels[0], 2000),
-        spinReel(reels[1], 3000),
-        spinReel(reels[2], 4000),
+        initializeReels();
 
-    ]);
+        const results = await Promise.all([
+            spinReel(reels[0], 2000),
+            spinReel(reels[1], 3000),
+            spinReel(reels[2], 4000),
 
-    handleButton.disabled = false;
+        ]);
 
-    //check if all results are the same
-    if(results[0] === results[1] && results[1] === results[2]) {
-        message.textContent = "YOU WIN !"
-        slotMachine.classList.add("win");
+        handleButton.disabled = false;
 
-        startConfetti();
-    } else{
-        message.textContent = "Try Again"
-        slotMachine.classList.add("loss");
+        //check if all results are the same
+        if (results[0] === results[1] && results[1] === results[2]) {
+            credits += bet*3;
+            message.textContent = `YOU WIN !`
+            slotMachine.classList.add("win");
+            availableCreditsDisplay.textContent = `Available credits: ${credits}`;
+
+            startConfetti();
+        } else {
+            credits-=bet;
+            console.log(credits);//debugging
+            
+            message.textContent = "Try Again"
+            slotMachine.classList.add("loss");
+            availableCreditsDisplay.textContent = `Available credits: ${credits}`;
+
+            if(credits <= 0){
+                window.alert("You Have Spent All Your Credits!")
+            }
+        }
     }
 }
 //initializing the reels on page load
-initializeReels();
 
 handleButton.addEventListener('click', startGame);
